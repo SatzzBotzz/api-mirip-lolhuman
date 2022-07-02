ff1bf9b733d2ad6db7048ead0ac4c3ae608bd292
SatzzBotzz <101557898+SatzzBotzz@users.noreply.github.com>
2022-07-02 08:46:23 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'ZeeoneOfc' // ubah jadi nama lu
-const listkey = ["Alphabot","Zeeone","ZeeoneOfc"]; // ubah apikey nya, tambah aja klo mau
+var creator = 'SatganzDevs' // ubah jadi nama lu
+const listkey = ["SatganzDevs","Satria","Satzz"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
 var qs = require('qs');
